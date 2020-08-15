3e430f938d6d4b904e6ec727e45001ba07098bb3
Jens Wetterich <jens@wetterich-net.de>
2020-08-15 17:13:24 +0200

    fix root path

--- main.py
+++ main.py
@@ -13,6 +13,8 @@ from service import create_item, delete_item, update_item, read_item, read_items
 model.Base.metadata.create_all(bind=model.engine)
 
 app = FastAPI(
+    root_path="/api/fastapi",
+    root_path_in_servers=False,
     servers=[
         {
             "url": "https://ovgu.jwet.de/api/spring",
